f9678cedb5a236c3e1ddeab0b5e197a4ffb1dea7
anemochore <fallensky@naver.com>
2020-05-07 16:59:03 +0900

    changed dataframes to scalars

--- excel/pandas_sum_average_multiple_workbooks.py
+++ excel/pandas_sum_average_multiple_workbooks.py
@@ -17,9 +17,9 @@ for workbook in all_workbooks:
 	worksheets_data_frame = None
 	workbook_data_frame = None
 	for worksheet_name, data in all_worksheets.items():
-		total_sales = pd.DataFrame([float(str(value).strip('$').replace(',','')) for value in data.ix[:, 'Sale Amount']]).sum()
+		total_sales = pd.DataFrame([float(str(value).strip('$').replace(',','')) for value in data.ix[:, 'Sale Amount']]).sum().iloc[0]
 		number_of_sales = len(data.loc[:, 'Sale Amount'])
-		average_sales = pd.DataFrame(total_sales / number_of_sales)
+		average_sales = total_sales / number_of_sales
 		
 		workbook_total_sales.append(total_sales)
 		workbook_number_of_sales.append(number_of_sales)
@@ -29,18 +29,18 @@ for workbook in all_workbooks:
 				'worksheet_total': total_sales,
 				'worksheet_average': average_sales}
 		
-		worksheet_data_frames.append(pd.DataFrame(data, columns=['workbook', 'worksheet', 'worksheet_total', 'worksheet_average']))
+		worksheet_data_frames.append(pd.DataFrame(data, columns=['workbook', 'worksheet', 'worksheet_total', 'worksheet_average'], index=[0]))
 	worksheets_data_frame = pd.concat(worksheet_data_frames, axis=0, ignore_index=True)
 
-	workbook_total = pd.DataFrame(workbook_total_sales).sum()
+	workbook_total = pd.DataFrame(workbook_total_sales).sum().iloc[0]
 	workbook_total_number_of_sales = pd.DataFrame(workbook_number_of_sales).sum()
-	workbook_average = pd.DataFrame(workbook_total / workbook_total_number_of_sales)
+	workbook_average = workbook_total / workbook_total_number_of_sales
 	
 	workbook_stats = {'workbook': os.path.basename(workbook),
 					 'workbook_total': workbook_total,
 					 'workbook_average': workbook_average}
 
-	workbook_stats = pd.DataFrame(workbook_stats, columns=['workbook', 'workbook_total', 'workbook_average'])
+	workbook_stats = pd.DataFrame(workbook_stats, columns=['workbook', 'workbook_total', 'workbook_average'], index=[0])
 	workbook_data_frame = pd.merge(worksheets_data_frame, workbook_stats, on='workbook', how='left')
 	data_frames.append(workbook_data_frame)
 
